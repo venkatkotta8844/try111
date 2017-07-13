a7b0c387fae72f26387a651906dfe3bce8bd22f1
Venkat Kotta <venkatkotta8844@gmail.com>
2017-07-13 15:46:36 -0400

    ch2

--- Add.java
+++ Add.java
@@ -1,16 +1,11 @@
 
-import java.util.*;
 class Add
 {
 public static void main(String s[])
 {
-int i,j,k;
-Scanner in = new Scanner(System.in);
-System.out.println("Enter first value =");
-i =in.nextInt();
+
 System.out.println("Enter Second value =");
-j = in.nextInt();
-k = i+j;
-System.out.println("The Addition of two numbers is = "+k);
+
+System.out.println("The Addition of two numbers is = ");
 }
 }
